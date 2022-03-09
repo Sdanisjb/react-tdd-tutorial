4b8809d5eb91ffeef54577a6187611ca392e10cf
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2022-03-09 18:30:36 -0500

    refactor(main-page test): Test mejorado para identificar el encabezado por rol

--- src/__tests__/main-page.test.js
+++ src/__tests__/main-page.test.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import MainPage from "../components/main-page";
+import { MainPage } from "../components/MainPage";
 
 describe("Main Page Mount", () => {
 	it("must display the main page title", () => {
 		render(<MainPage />);
-		expect(screen.getByText(/simpsons quotes/i)).toBeInTheDocument();
+		expect(
+			screen.getByRole("heading", { name: /simpsons quotes/i })
+		).toBeInTheDocument();
 	});
 });
