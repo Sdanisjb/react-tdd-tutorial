24361406d620f91dc3bda472fa9d5f00283f32d7
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2022-03-09 18:50:44 -0500

    refactor (MainPage Test): Renderizado del componente en cada test. Declaración del comportamiento antes de los tests.

--- src/__tests__/main-page.test.js
+++ src/__tests__/main-page.test.js
@@ -3,9 +3,10 @@ import { render, screen } from "@testing-library/react";
 
 import { MainPage } from "../components/MainPage";
 
+beforeEach(() => render(<MainPage />));
+
 describe("Main Page Mount", () => {
 	it("must display the main page title", () => {
-		render(<MainPage />);
 		expect(
 			screen.getByRole("heading", { name: /simpsons quotes/i })
 		).toBeInTheDocument();
@@ -14,7 +15,6 @@ describe("Main Page Mount", () => {
 
 describe("Quotes list", () => {
 	it("must display 3 quotes", async () => {
-		render(<MainPage />);
 		expect(await screen.findAllByRole("listitem")).toHaveLength(3);
 	});
 });
