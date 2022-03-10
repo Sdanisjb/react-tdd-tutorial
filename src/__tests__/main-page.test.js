2c5bdab25b6520c62e1704b3a1d0865a1d3f6436
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2022-03-09 20:18:07 -0500

    chore (package): Se agregó la librería msw para Mock Servers

--- src/__tests__/main-page.test.js
+++ src/__tests__/main-page.test.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-
+import {
+	render,
+	screen,
+	waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 import { MainPage } from "../components/MainPage";
+
+const fakeQuotes = [
+	{ quote: "Gah, stupid sexy Flanders!" },
+	{ quote: "Eat my shorts" },
+	{ quote: "Shut up, brain. I got friends now. I don't need you anymore" },
+];
+
+const server = setupServer(
+	rest.get("/quotes", (req, res, ctx) => {
+		return res(ctx.json(fakeQuotes));
+	})
+);
+
+beforeAll(() => server.listen());
+
+afterAll(() => server.close());
 
 beforeEach(() => render(<MainPage />));
 
 describe("Main Page Mount", () => {
-	it("must display the main page title", () => {
+	it("must display the main page title", async () => {
 		expect(
 			screen.getByRole("heading", { name: /simpsons quotes/i })
 		).toBeInTheDocument();
+		await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 	});
 });
 
@@ -22,10 +44,10 @@ describe("Quotes list", () => {
 		const [firstQuote, secondQuote, thirdQuote] = await screen.findAllByRole(
 			"listitem"
 		);
-		expect(firstQuote.textContent).toBe("Gah, stupid sexy Flanders!");
-		expect(secondQuote.textContent).toBe("Eat my shorts");
-		expect(thirdQuote.textContent).toBe(
-			"Shut up, brain. I got friends now. I don't need you anymore"
-		);
+
+		const [fakeOne, fakeTwo, fakeThree] = fakeQuotes;
+		expect(firstQuote.textContent).toBe(fakeOne.quote);
+		expect(secondQuote.textContent).toBe(fakeTwo.quote);
+		expect(thirdQuote.textContent).toBe(fakeThree.quote);
 	});
 });
